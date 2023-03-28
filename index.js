be75ec26ba028f509334afa02eefe0f406bdf97d
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-03-28 14:17:07 +0530

    added routes

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+//if we use the any console function here as a middlewere 
+app.use((req,res,next)=>{
+    console.log("Route",req.url, "Method",req.method, "At",new Date())
+    //after the console use next function hence it's not showing error.
+    next()
+})
+
 app.use("/api/product/",productRoute);
 
 app.get('/',(req,res)=>res.send('Hello'));
--- routes/product.route.js
+++ routes/product.route.js
@@ -2,7 +2,8 @@
 const express=require('express')
 const productRoute=express.Router();
 
-productRoute.get("/",(req,res,next)=>{res.send("Welcome to Product")})
+productRoute.get("/",(req,res,next)=>{res.send("Welcome to Product !")});
+productRoute.delete("/productid",(req,res,next)=>{res.send("Product Deleted !")})
 
 
 module.exports=productRoute;
